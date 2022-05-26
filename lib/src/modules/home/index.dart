4336cddf8183b1ffa51fe72bac971a15090cb1da
Piyush Bhatia <Piyush_Bhatia@epam.com>
2022-05-26 14:33:04 +0530

    Adding changes

--- lib/src/modules/home/index.dart
+++ lib/src/modules/home/index.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_server_driven_ui/src/modules/home/models/fieldsModel.dart';
 import 'package:flutter_server_driven_ui/src/modules/home/models/formModel.dart';
 import 'package:flutter_server_driven_ui/src/modules/home/wodgets/form/index.dart';
+import 'package:flutter_server_driven_ui/src/modules/second/index.dart';
 
 class HomePage extends StatefulWidget{
   HomePageState createState()=>HomePageState();
@@ -14,7 +15,11 @@ class HomePageState extends State<HomePage>{
       appBar: AppBar(
         title: const Text("Server Driven UI"),
       ),
-      body: FormWidget(),
+      body: ListView(children:[ FormWidget(),Center(
+        child: ElevatedButton(onPressed: (){
+          Navigator.push(context,MaterialPageRoute(builder: (context) => SecondPage()));
+        },child: const Center(child: Text("Navigate to Next Page"),)),
+      )]),
     );
   }
 }
